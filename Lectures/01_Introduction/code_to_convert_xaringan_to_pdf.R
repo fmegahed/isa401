d697f320e576c499b7ebd3ddbe96b5df7942f1a7
Fadel Megahed <fmegahed@miamioh.edu>
2022-01-21 08:41:20 -0500

    Reorganized location of lecture files

---
+++ Lectures/01_Introduction/code_to_convert_xaringan_to_pdf.R
@@ -0,0 +1,7 @@
+# Based on this awesome ref
+# https://www.garrickadenbuie.com/blog/print-xaringan-chromote/
+
+source("https://git.io/xaringan2pdf")
+
+xaringan_to_pdf(input = 'https://fmegahed.github.io/isa401/01_Introduction/01_Introduction.html',
+                output_file = 'Lectures/01_Introduction/01_Introduction.pdf')
