e513e13e8a5dbf47085c11a2887c372320684b92
fmegahed <fmegahed@miamioh.edu>
2022-09-28 07:26:17 -0400

    isa401 fall22 class11

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -1,15 +1,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/09_technically_correct_and_consistent_data/09_technically_correct_and_consistent_data.html',
-                   to = 'pdfs/09_technically_correct_and_consistent_data.pdf',
+renderthis::to_pdf(from = 'lectures/11_data_correction_and_imputation/11_data_correction_and_imputation.html',
+                   to = 'pdfs/11_data_correction_and_imputation.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 2)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/09_technically_correct_and_consistent_data/09_technically_correct_and_consistent_data.html',
-                    to = 'ppts/09_technically_correct_and_consistent_data.pptx',
+renderthis::to_pptx(from = 'lectures/11_data_correction_and_imputation/11_data_correction_and_imputation.html',
+                    to = 'ppts/11_data_correction_and_imputation.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 2)
