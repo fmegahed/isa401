8ed59cc21ad8ba48f0777102fa2dd9b958c89b8e
Fadel Megahed <fmegahed@miamioh.edu>
2024-04-16 09:28:03 -0400

    isa401 spring2024 class21

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -1,15 +1,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/20_high_dimensional_charts/20_high_dimensional_charts.html',
-                   to = 'pdfs/20_high_dimensional_charts.pdf',
+renderthis::to_pdf(from = 'lectures/21_business_reports/21_business_reports.html',
+                   to = 'pdfs/21_business_reports.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 1)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/20_high_dimensional_charts/20_high_dimensional_charts.html',
-                    to = 'ppts/20_high_dimensional_charts.pptx',
+renderthis::to_pptx(from = 'lectures/21_business_reports/21_business_reports.html',
+                    to = 'ppts/21_business_reports.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 1)
